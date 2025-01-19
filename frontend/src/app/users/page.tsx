e97526b930c98ec2e21db4a8398556c6cdc55ed9
budhil <budhilnigam@gmail.com>
2025-01-20 02:39:04 +0530

    Basic 3d scene

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
 
-const UsersPage = () => {
+import { FC, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
+
+const RotatingCube: FC = () => {
+  const cubeRef = useRef<Mesh>(null);
+
+  useFrame(() => {
+    if (cubeRef.current) {
+      cubeRef.current.rotation.x += 0.01;
+      cubeRef.current.rotation.z += 0.01;
+    }
+  });
+
   return (
-    <div>page</div>
-  )
-}
+    <mesh ref={cubeRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="#2f74c0" />
+    </mesh>
+  );
+};
 
-export default UsersPage
+const Scene: FC = () => {
+  return (
+    <Canvas style={{ background: 'black' }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <RotatingCube />
+    </Canvas>
+  );
+};
+
+const UsersPage: FC = () => {
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <Scene />
+    </div>
+  );
+};
+
+export default UsersPage;
